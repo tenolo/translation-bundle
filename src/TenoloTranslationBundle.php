3b92bf6c55893c5149cfd2c1455944e998080293
Nikita Loges <git@nikita-loges.de>
2016-06-02 16:30:52 +0200

    bundle dependency

--- src/TenoloTranslationBundle.php
+++ src/TenoloTranslationBundle.php
@@ -2,7 +2,17 @@
 
 namespace Tenolo\Bundle\TranslationBundle;
 
+use Mmoreram\SymfonyBundleDependencies\DependentBundleInterface;
+use Shapecode\Bundle\RasSBundle\ShapecodeRasSBundle;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\TwigBundle\TwigBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Tenolo\Bundle\AdminControlPanelBundle\TenoloAdminControlPanelBundle;
+use Tenolo\Bundle\CoreBundle\TenoloCoreBundle;
+use Tenolo\Bundle\CRUDAdminBundle\TenoloCRUDAdminBundle;
+use Tenolo\Bundle\DoctrineTablePrefixBundle\TenoloDoctrineTablePrefixBundle;
+use Tenolo\Bundle\MenuBundle\TenoloMenuBundle;
 
 /**
  * Class TenoloTranslationBundle
@@ -10,6 +20,23 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  * @author Nikita Loges
  * @company tenolo GbR
  */
-class TenoloTranslationBundle extends Bundle
+class TenoloTranslationBundle extends Bundle implements DependentBundleInterface
 {
+
+    /**
+     * @inheritdoc
+     */
+    public static function getBundleDependencies(KernelInterface $kernel)
+    {
+        return [
+            FrameworkBundle::class,
+            TwigBundle::class,
+            ShapecodeRasSBundle::class,
+            TenoloCoreBundle::class,
+            TenoloMenuBundle::class,
+            TenoloCRUDAdminBundle::class,
+            TenoloAdminControlPanelBundle::class,
+            TenoloDoctrineTablePrefixBundle::class,
+        ];
+    }
 }
