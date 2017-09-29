2c45cb68e43dc7f3d529d6e201b134cae2b62f97
Nikita Loges <git@nikita-loges.de>
2017-09-29 10:51:49 +0200

    menu stuff

--- src/EventListener/MenuStructureListener.php
+++ src/EventListener/MenuStructureListener.php
@@ -8,10 +8,10 @@ use Tenolo\Bundle\MenuBundle\EventListener\BaseMenuStructureListener;
 
 /**
  * Class MenuStructureListener
+ *
  * @package Tenolo\Bundle\TranslationBundle\EventListener
- * @author Nikita Loges
+ * @author  Nikita Loges
  * @company tenolo GbR
- * @date 16.01.2015
  */
 class MenuStructureListener extends BaseMenuStructureListener
 {
@@ -43,64 +43,64 @@ class MenuStructureListener extends BaseMenuStructureListener
      */
     protected function addLanguageItems(ItemInterface $root)
     {
-        $singular = $this->getEntityTranslation('translation.language.object.singular');
-        $plural = $this->getEntityTranslation('translation.language.object.plural');
+        $singular = $this->translator->trans('translation.language.object.singular', [], 'entities');
+        $plural = $this->translator->trans('translation.language.object.plural', [], 'entities');
 
         $child = $this->addRootItem($root, $plural, 'flag');
 
         $this->addHeader($child, $plural);
         $this->addListEntityItem($child, $plural, 'acp.translation.language.list');
-        $this->addAddEntityItem($child, $singular, 'acp.translation.language.add', array(), array(
-            'extras' => array(
-                'routes' => array(
+        $this->addAddEntityItem($child, $singular, 'acp.translation.language.add', [], [
+            'extras' => [
+                'routes' => [
                     'acp.translation.language.edit'
-                )
-            )
-        ));
+                ]
+            ]
+        ]);
 
-        $singular = $this->getEntityTranslation('translation.domain.object.singular');
-        $plural = $this->getEntityTranslation('translation.domain.object.plural');
+        $singular = $this->translator->trans('translation.domain.object.singular', [], 'entities');
+        $plural = $this->translator->trans('translation.domain.object.plural', [], 'entities');
 
         $this->getMenuBuilderService()->addDivider($child);
 
         $this->addHeader($child, $plural);
         $this->addListEntityItem($child, $plural, 'acp.translation.domain.list');
-        $this->addAddEntityItem($child, $singular, 'acp.translation.domain.add', array(), array(
-            'extras' => array(
-                'routes' => array(
+        $this->addAddEntityItem($child, $singular, 'acp.translation.domain.add', [], [
+            'extras' => [
+                'routes' => [
                     'acp.translation.domain.edit'
-                )
-            )
-        ));
+                ]
+            ]
+        ]);
 
-        $singular = $this->getEntityTranslation('translation.token.object.singular');
-        $plural = $this->getEntityTranslation('translation.token.object.plural');
+        $singular = $this->translator->trans('translation.token.object.singular', [], 'entities');
+        $plural = $this->translator->trans('translation.token.object.plural', [], 'entities');
 
         $this->getMenuBuilderService()->addDivider($child);
 
         $this->addHeader($child, $plural);
         $this->addListEntityItem($child, $plural, 'acp.translation.token.list');
-        $this->addAddEntityItem($child, $singular, 'acp.translation.token.add', array(), array(
-            'extras' => array(
-                'routes' => array(
+        $this->addAddEntityItem($child, $singular, 'acp.translation.token.add', [], [
+            'extras' => [
+                'routes' => [
                     'acp.translation.token.edit'
-                )
-            )
-        ));
+                ]
+            ]
+        ]);
 
-        $singular = $this->getEntityTranslation('translation.object.singular');
-        $plural = $this->getEntityTranslation('translation.object.plural');
+        $singular = $this->translator->trans('translation.object.singular', [], 'entities');
+        $plural = $this->translator->trans('translation.object.plural', [], 'entities');
 
         $this->getMenuBuilderService()->addDivider($child);
 
         $this->addHeader($child, $plural);
         $this->addListEntityItem($child, $plural, 'acp.translation.list');
-        $this->addAddEntityItem($child, $singular, 'acp.translation.add', array(), array(
-            'extras' => array(
-                'routes' => array(
+        $this->addAddEntityItem($child, $singular, 'acp.translation.add', [], [
+            'extras' => [
+                'routes' => [
                     'acp.translation.edit'
-                )
-            )
-        ));
+                ]
+            ]
+        ]);
     }
 }
