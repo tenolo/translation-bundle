33f81277463ed517730d2fd93d765f731e0178ce
Nikita Loges <git@nikita-loges.de>
2018-08-10 11:21:05 +0200

    remove dependent bundle

--- src/TenoloTranslationBundle.php
+++ src/TenoloTranslationBundle.php
@@ -2,37 +2,15 @@
 
 namespace Tenolo\Bundle\TranslationBundle;
 
-use Mmoreram\SymfonyBundleDependencies\DependentBundleInterface;
-use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
-use Symfony\Bundle\TwigBundle\TwigBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\HttpKernel\KernelInterface;
-use Tenolo\Bundle\AdminControlPanelBundle\TenoloAdminControlPanelBundle;
-use Tenolo\Bundle\CoreBundle\TenoloCoreBundle;
-use Tenolo\Bundle\DoctrineTablePrefixBundle\TenoloDoctrineTablePrefixBundle;
-use Tenolo\Bundle\MenuBundle\TenoloMenuBundle;
 
 /**
  * Class TenoloTranslationBundle
+ *
  * @package Tenolo\Bundle\TranslationBundle
- * @author Nikita Loges
+ * @author  Nikita Loges
  * @company tenolo GbR
  */
-class TenoloTranslationBundle extends Bundle implements DependentBundleInterface
+class TenoloTranslationBundle extends Bundle
 {
-
-    /**
-     * @inheritdoc
-     */
-    public static function getBundleDependencies(KernelInterface $kernel)
-    {
-        return [
-            FrameworkBundle::class,
-            TwigBundle::class,
-            TenoloCoreBundle::class,
-            TenoloMenuBundle::class,
-            TenoloAdminControlPanelBundle::class,
-            TenoloDoctrineTablePrefixBundle::class,
-        ];
-    }
 }
